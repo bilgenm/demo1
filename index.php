0e33eb7bf34b7aed2c1c8068718bdab55d00fd9d
bilgenm <bilgen.mustafa@condorcet.be>
2014-09-26 11:29:26 +0200

    Connexion ajoute

--- index.php
+++ index.php
@@ -5,7 +5,13 @@
     </head>
     <body>
         <?php
-        print "Hello";
+        $connexion = "host=localhost password=840919 user=bilgen  port=5432";
+        $cnx = pg_connect($connexion);
+        if ($cnx) {
+            print "OK";
+        } else {
+            print "Echec";
+        }
         ?>
     </body>
 </html>
